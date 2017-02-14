2beb44089d81d4d8b3518e3402946344c2cf942a
kozachello <atkozak2011@gmail.com>
2017-02-14 15:02:28 +0100

    little fx

--- src/main/java/home/client/ClientView.java
+++ src/main/java/home/client/ClientView.java
@@ -1,6 +1,9 @@
 package home.client;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -11,6 +14,17 @@ public class ClientView extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        primaryStage.setTitle("fx");
+        Pane pane = new Pane();
+        Text text = new Text("hello");
+        pane.getChildren().add(text);
+        Scene scene = new Scene(pane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
 
+    }
+
+    public static void main(String args[]) {
+        launch(args);
     }
 }
