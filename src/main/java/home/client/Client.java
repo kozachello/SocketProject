0af1b2a6b892b91a1333ef75c028895e1d8aaa7b
kozachello <atkozak2011@gmail.com>
2017-02-21 11:24:56 +0100

    layout and client

--- src/main/java/home/client/Client.java
+++ src/main/java/home/client/Client.java
@@ -6,4 +6,18 @@ package home.client;
 
 public class Client {
 
+    private String username;
+
+    public Client(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 }
--- src/main/java/home/client/ClientView.java
+++ src/main/java/home/client/ClientView.java
@@ -1,7 +1,11 @@
 package home.client;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -14,11 +18,15 @@ public class ClientView extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        primaryStage.setTitle("fx");
-        Pane pane = new Pane();
-        Text text = new Text("hello");
-        pane.getChildren().add(text);
-        Scene scene = new Scene(pane);
+        primaryStage.setTitle("application");
+        GridPane grid = new GridPane();
+        TextField text = new TextField();
+        text.setAlignment(Pos.CENTER);
+        Button button = new Button("start");
+        button.setAlignment(Pos.CENTER);
+        grid.add(text, 2, 2);
+        grid.add(button, 4, 2);
+        Scene scene = new Scene(grid, 280, 100);
         primaryStage.setScene(scene);
         primaryStage.show();
 
